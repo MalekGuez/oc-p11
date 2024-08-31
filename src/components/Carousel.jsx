b848ccdbe75c0bce49f6225a1fc6b949b8b403d7
Malek <rogex2542@gmail.com>
2024-08-31 14:27:12 +0200

    last commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,20 +19,25 @@ const Carousel = ({ images }) => {
         <div className="kasa-carousel">
             <div className="kasa-carousel__content">
                 <img className="kasa-carousel__image" src={images[currentImageIndex]} alt="Image"/>
-  
-                <button className="kasa-carousel__arrow kasa-carousel__arrow--left" onClick={handlePrev}>
-                    <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
-                    </svg>
-                </button>
-                <button className="kasa-carousel__arrow kasa-carousel__arrow--right" onClick={handleNext}>
-                    <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
-                    </svg>
-                </button>
+
+                {images.length > 1 && (
+                    <>
+                        <button className="kasa-carousel__arrow kasa-carousel__arrow--left" onClick={handlePrev}>
+                            <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
+                            </svg>
+                        </button>
+                        <button className="kasa-carousel__arrow kasa-carousel__arrow--right" onClick={handleNext}>
+                            <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
+                            </svg>
+                        </button>
+                    </>
+                )}
             </div>
-  
-            <div className="kasa-carousel__counter">{`${currentImageIndex + 1} / ${images.length}`}</div>
+            {images.length > 1 && (
+                <div className="kasa-carousel__counter">{`${currentImageIndex + 1} / ${images.length}`}</div>
+            )}
         </div>
     );
   };
